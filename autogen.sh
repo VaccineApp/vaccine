e658c37547b101b16965f29edb2ae3a2a96acb9c
Ben Iofel <iofelben+github@gmail.com>
2015-12-24 13:29:04 -0500

    improve autotools a bit

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,10 @@
-#!/bin/sh
+#!/bin/sh -e
+
 srcdir=`dirname $0`
-[ -z "$srcdir" ] && srcdir=.
+test -n "$srcdir" || srcdir=.
 
-PKG_NAME=vaccine
+(cd "$srcdir"; autoreconf -i)
 
-if [ ! -f "$srcdir/configure.ac" ]; then
- echo "$srcdir doesn't look like source directory for $PKG_NAME" >&2
- exit 1
+if test -z "$NOCONFIGURE"; then
+    "$srcdir"/configure "$@"
 fi
-
-which gnome-autogen.sh || {
-        echo "You need to install gnome-common from GNOME Git"
-        exit 1
-}
-
-. gnome-autogen.sh "$@"
