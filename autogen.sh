6c204d88c2e0129a13cd1d043ba4b3d05faafb77
Ben Iofel <iofelben@gmail.com>
2016-04-27 11:53:31 -0400

    simpler autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,7 @@
 #!/bin/sh -e
 
-srcdir=`dirname $0`
-test -n "$srcdir" || srcdir=.
-
-(cd "$srcdir"; autoreconf -i)
-
-if test -z "$NOCONFIGURE"; then
-    "$srcdir"/configure "$@"
+if ! test -f contrib/bayes-glib/README.md; then
+    git submodule update --init
 fi
+
+autoreconf -if
